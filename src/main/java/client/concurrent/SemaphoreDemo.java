f0470f491c39fe37f9954ec71b1588ad8098feb3
wangsongpeng <wangsongpeng@jd.com>
2018-01-04 19:30:42 +0800

    Semaphore learn

---
+++ src/main/java/client/concurrent/SemaphoreDemo.java
@@ -0,0 +1,38 @@
+package client.concurrent;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+/**
+ * 信号量同步工具类
+ * @author wsp
+ * @since 2018/01/04
+ */
+public class SemaphoreDemo {
+
+   private static final int THREAD_COUNT = 30;
+
+   private static  Semaphore semaphore = new Semaphore(10);
+
+    public static void main(String[] args) {
+        for(int i = 0; i < THREAD_COUNT ; i++){
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        /*尝试获得许可证*/
+                        if(semaphore.tryAcquire()){
+                            System.out.println("Thread Name :" +Thread.currentThread().getName() + " Save Data Ing...!");
+                        }else{
+                            System.out.println("Thread Name :" +Thread.currentThread().getName() + " Can't Execute!");
+                        }
+                    }catch (Exception e){
+                        e.printStackTrace();
+                    }
+                }
+            },"wsp-"+i).start();
+        }
+    }
+
+}
